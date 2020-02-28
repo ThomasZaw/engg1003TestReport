596ae24c8be3f6029b1277925c35c610cb3d9914
Thiha Thet Zaw <c3341498@uon.edu.au>
2020-02-28 14:43:01 +1100

    commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int k;
+    printf("enter you number: ");//Type any interger here
+    scanf("%d",&k);
+    k=2*k;//total value of k
+    printf("The number is: %d\n", k);//total number of result
+    return 0;
+}
